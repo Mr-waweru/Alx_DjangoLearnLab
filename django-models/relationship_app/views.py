36d9f3084eb92d3fd9119386e17a02498ff88640
Mr-waweru <gitaugeorge001@gmail.com>
2024-11-09 00:02:52 +0300

    Completed task 2

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,25 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from .models import *
 
 # Create your views here.
+def list_all_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {
+        "books": books
+    })
+
+
+# Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
+# Utilize Django’s ListView or DetailView to structure this class-based view.
+class LibraryDetail(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context =  super().get_context_data(**kwargs)
+        context["books"] = self.object.books.all()
+        return context
