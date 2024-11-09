d01a169769d28242e6bc92a4f2c1732a80b557da
Mr-waweru <gitaugeorge001@gmail.com>
2024-11-10 01:24:07 +0300

    Modified urls to include template name

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,7 +6,7 @@ from .views import list_books, LibraryDetailView, login_view, logout_view, regis
 urlpatterns = [
     path("books", views.list_books, name="list_books"),
     path("library/<int:pk>", views.LibraryDetailView.as_view(), name="library_detail"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
+    path('login/', views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path("register", views.register, name="register")
 ]
