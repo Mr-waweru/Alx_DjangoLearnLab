3467a82e18a588d2a4e691acfcab00c5b3136665
Mr-waweru <gitaugeorge001@gmail.com>
2024-11-10 01:15:46 +0300

    Completed tast 2

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, login_view, logout_view, register
 
 urlpatterns = [
     path("books", views.list_books, name="list_books"),
-    path("library/<int:pk>", views.LibraryDetailView.as_view(), name="library_detail")
+    path("library/<int:pk>", views.LibraryDetailView.as_view(), name="library_detail"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register")
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,12 +2,17 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 from .models import Book
 from .models import Library
 
 # Create your views here.
-def list_all_books(request):
+def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {
         "books": books
@@ -16,7 +21,7 @@ def list_all_books(request):
 
 # Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
 # Utilize Django’s ListView or DetailView to structure this class-based view.
-class LibraryDetail(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
@@ -25,3 +30,40 @@ class LibraryDetail(DetailView):
         context =  super().get_context_data(**kwargs)
         context["books"] = self.object.books.all()
         return context
+    
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("login"))
+    else:
+        form = UserCreationForm()
+
+    return render(request, "relationship_app/register.html",{
+        "form": form
+    })
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse(""))
+        else:
+            return render(request, "relationship_app/login.html",{
+                "message": "Invalid Credentials."
+            })
+        
+    return render(request, "relationship_app/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/login.html", {
+        "message": "Logged out"
+    })
