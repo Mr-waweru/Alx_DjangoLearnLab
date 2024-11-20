b2593d2ef086e06dc5435c1060ef6bdcda136a87
Mr-waweru <gitaugeorge001@gmail.com>
2024-11-21 00:27:19 +0300

    Added router.urls to handle all CRUD operations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,25 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+# Create the router
+router = DefaultRouter()
+
+# Register the BookViewSet
+router.register(r"books_all", views.BookViewSet, basename="book_all")   # The r in r'books_all' denotes a raw string in Python. Without the r, '\' would be interpreted as a newline character.
+views.BookViewSet
 urlpatterns = [
-    path("books/", views.BookList.as_view(), name="book-list")  # Maps to the BookList view
+    path("books/", views.BookList.as_view(), name="book-list"),  # Maps to the BookList view
+    path("", include(router.urls)),
 ]
+
+"""
+Automatically generates URL patterns for all CRUD operations,
+No need to manually define paths for CRUD operations.:
+-> GET /api/books_all/: List all books.
+-> POST /api/books_all/: Create a new book.
+-> GET /api/books_all/<id>/: Retrieve a specific book by its ID.
+-> PUT /api/books_all/<id>/: Update a book.
+-> DELETE /api/books_all/<id>/: Delete a book.
+"""
