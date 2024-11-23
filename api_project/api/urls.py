d02bd4fec9bde2a86deeae90975451cab706c757
Mr-waweru <gitaugeorge001@gmail.com>
2024-11-24 00:16:13 +0300

    Provide a way for users to obtain a token and use it for authentication

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
@@ -12,6 +13,7 @@ views.BookViewSet
 urlpatterns = [
     path("books/", views.BookList.as_view(), name="book-list"),  # Maps to the BookList view
     path("", include(router.urls)),
+    path("api-token-auth/", obtain_auth_token, name="api-token-auth")
 ]
 
 """
