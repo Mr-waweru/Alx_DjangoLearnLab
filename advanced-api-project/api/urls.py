f68dba6a0c423e6ab810207985ce978d803cc1d5
Mr-waweru <gitaugeorge001@gmail.com>
2024-11-29 02:46:27 +0300

    Configure url patterns to connect the views

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("books/", views.ListView.as_view(), name="book_list"),
+    path("books/<int:pk>/", views.DetailView.as_view(), name="book_detail"),
+    path("books/create/", views.CreateView.as_view(), name="book_create"),
+    path('books/update/<int:pk>/', views.UpdateView.as_view(), name='book_update'),
+    path('books/delete/<int:pk>/', views.DeleteView.as_view(), name='book_delete'),  
+]
