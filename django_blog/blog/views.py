df89ce7b9d4654039d3c9b169853694f96204b3b
Mr-waweru <gitaugeorge001@gmail.com>
2024-12-08 23:21:34 +0300

    Completed task 1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-# Create your views here.
+# User Registration View
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = UserCreationForm()
+    return render(request, "blog/register.html", {"form": form})
+
+# User Login View
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "blog/login.html", {"form": form})
+
+# User Logout View
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+# Profile Management View
+@login_required
+def profile(request):
+    return render(request, "blog/profile.html", {"user": request.user})
