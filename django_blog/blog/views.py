c00b39178f84d567daf1a244bea49630bf4b14db
Mr-waweru <gitaugeorge001@gmail.com>
2024-12-08 23:29:14 +0300

    Refactor task 1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.contrib import messages
 
 # User Registration View
 def register(request):
@@ -11,7 +11,10 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, "You have successfully registered and are now logged in.")
             return redirect("profile")
+        else:
+            messages.error(request, "There was an error with your registration. Please try again.")
     else:
         form = UserCreationForm()
     return render(request, "blog/register.html", {"form": form})
@@ -23,7 +26,10 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            messages.success(request, "You are now logged in.")
             return redirect("profile")
+        else:
+            messages.error(request, "Invalid credentials. Please try again.")
     else:
         form = AuthenticationForm()
     return render(request, "blog/login.html", {"form": form})
@@ -31,6 +37,7 @@ def user_login(request):
 # User Logout View
 def user_logout(request):
     logout(request)
+    messages.success(request, "You have successfully logged out.")
     return redirect("login")
 
 # Profile Management View
