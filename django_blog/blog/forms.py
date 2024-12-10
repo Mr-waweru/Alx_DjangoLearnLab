edb8a28ee6aca3a8d0406311c31d0b33a6bc3669
Mr-waweru <gitaugeorge001@gmail.com>
2024-12-10 17:12:49 +0300

    Refactored

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,4 +1,38 @@
 from django import forms
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from .models import Comment, Post
+from taggit.forms import TagWidget  # Import TagWidget for tags
+
+User = get_user_model()
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your comment here...'}),
+        }
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']  # Include 'tags' field
+        widgets = {
+            'tags': TagWidget(),  # Use TagWidget for tags
+        }
+
+
+"""
+from django import forms
 from .models import Post, Comment
 from taggit.forms import TagField, TagWidget
 
@@ -33,3 +67,4 @@ class CommentForm(forms.ModelForm):
                 attrs={"class": "form-control", "placeholder": "Add a comment...", "rows": 3}
             ),
         }
+"""
