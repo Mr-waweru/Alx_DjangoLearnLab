4a4621e088de6e7f32b0f727565a663437c37e79
Mr-waweru <gitaugeorge001@gmail.com>
2024-12-17 15:49:00 +0300

    Add follow and unfollow paths

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("profile/", views.UserDetailView.as_view(), name="profile"),
-    path("follow/<int:pk>/", views.FollowUserView.as_view(), name="follow_user"),   # <int:user_id>
-    path("unfollow/<int:pk>/", views.UnfollowUserView.as_view(), name="unfollow_user"),  # <int:user_id>
+    path("follow/<int:user_id>/", views.FollowUserView.as_view(), name="follow_user"),   # <int:pk>
+    path("unfollow/<int:user_id>/", views.UnfollowUserView.as_view(), name="unfollow_user"),  # <int:pk>
 ]
